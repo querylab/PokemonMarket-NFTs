c81a960247c89f1639d645486d9d460602cddc23
querylab <emygonzalez@tutanota.com>
2023-10-06 17:13:20 -0400

    umami+ackeefix

--- pages/_app.js
+++ pages/_app.js
@@ -9,16 +9,14 @@ import { Navbar, Footer } from '../components';
 
 const App = ({ Component, pageProps }) => (
   <NFTProvider>
-<script async src="https://umami.thatquery.com/script.js" data-website-id="1ae40614-3789-4492-9576-5a177f88f5d0"></script>
-
-<script defer data-domain="pokemonmarket.com" src="http://plausible.thatquery.com/js/script.js"></script>
+        <script async src="https://umami.thatquery.com/script.js" data-website-id="4f42dcac-84bd-4099-a700-5992e6443484"></script>
+        <script async src="https://ackee.thatquery.com/tracker.js" data-ackee-server="https://ackee.thatquery.com" data-ackee-domain-id="4855914a-3ec9-4de6-bfb6-39e9cb8ad678"></script>
 
     <ThemeProvider attribute="class">
       <div className="dark:bg-nft-dark bg-white min-h-screen">
 
-      <script defer data-domain="pokemonmarket.com" src="http://plausible.thatquery.com/js/script.js"></script>
-
-      <script async src="https://umami.thatquery.com/script.js" data-website-id="1ae40614-3789-4492-9576-5a177f88f5d0"></script>
+      <script async src="https://umami.thatquery.com/script.js" data-website-id="4f42dcac-84bd-4099-a700-5992e6443484"></script>
+      <script async src="https://ackee.thatquery.com/tracker.js" data-ackee-server="https://ackee.thatquery.com" data-ackee-domain-id="4855914a-3ec9-4de6-bfb6-39e9cb8ad678"></script>
 
         <Head>
           <title>pokemonmarket.com</title>
@@ -29,9 +27,8 @@ const App = ({ Component, pageProps }) => (
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <link rel="icon" href="/favicon.ico" />
 
-      <script async src="https://umami.thatquery.com/script.js" data-website-id="1ae40614-3789-4492-9576-5a177f88f5d0"></script>
-
-      <script defer data-domain="pokemonmarket.com" src="http://plausible.thatquery.com/js/script.js"></script>
+          <script async src="https://umami.thatquery.com/script.js" data-website-id="4f42dcac-84bd-4099-a700-5992e6443484"></script>
+          <script async src="https://ackee.thatquery.com/tracker.js" data-ackee-server="https://ackee.thatquery.com" data-ackee-domain-id="4855914a-3ec9-4de6-bfb6-39e9cb8ad678"></script>
 
 
          </Head>
